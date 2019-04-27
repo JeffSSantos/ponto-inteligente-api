31da9403f76c1f4bd796a2979cc93c896aee8447
JeffSSantos <jefter_santana@hotmail.com>
2019-04-27 15:17:04 -0300

    Criado servico para cadastro de pessoa juridica.

--- src/main/java/com/jefter/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/jefter/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,12 +2,12 @@ package com.jefter.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
-
 }
