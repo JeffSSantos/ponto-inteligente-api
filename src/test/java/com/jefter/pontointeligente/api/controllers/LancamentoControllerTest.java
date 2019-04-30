5a5c9f19b5f26e3a74f9b216aa84a52f56d9c72a
jeftersantoszup <jefter.santos@zup.com.br>
2019-04-30 13:35:44 -0300

    Implementado autenticacao por token JWT.
    Signed-off-by: JeffSSantos <jefter_santana@hotmail.com>

--- src/test/java/com/jefter/pontointeligente/api/controllers/LancamentoControllerTest.java
+++ src/test/java/com/jefter/pontointeligente/api/controllers/LancamentoControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -65,6 +66,7 @@ public class LancamentoControllerTest {
 
 
 	@Test
+	@WithMockUser
 	public void testCadastrarLancamento() throws Exception {
 		Lancamento lancamento = obterDadosLancamento();
 		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Funcionario()));
@@ -83,6 +85,7 @@ public class LancamentoControllerTest {
 	}
 
 	@Test
+	@WithMockUser
 	public void testCadastrarLancamentoFuncionarioIdInvalido() throws Exception {
 		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.empty());
 
@@ -96,6 +99,7 @@ public class LancamentoControllerTest {
 	}
 
 	@Test
+	@WithMockUser(username = "admin@admin.com", roles = {"ADMIN"})
 	public void testRemoverLancamento() throws Exception {
 		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
 
